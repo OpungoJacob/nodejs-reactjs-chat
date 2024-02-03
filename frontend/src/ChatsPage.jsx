ff51c073996d4d58248f12ca56c74c1c4397a53f
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-02-03 12:37:54 +0300

    new

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -24,10 +24,11 @@ const ChatsPage = (props) =>{
     return (
         <div style={{height: '100vh'}}>
             <PrettyChatWindow
-                projectid=''
-                username=''
-                secret=''
+                projectid='0d7d730e-5dc0-400b-989a-f62b9809f126'
+                username={props.user.username}
+                secret={props.user.secret}
                 style={{height: '100%'}}
+            />
         </div>
     )
 }
