34b9bbd882d3238777042f8cd302392d7db5654a
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-02-03 10:54:48 +0300

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
+import "./App.css";
+
+import AuthPage from "./AuthPage";
+import ChatsPage from "./ChatsPage";
 
 function App() {
+
+  const [user, setuser] = useState(undefined);
+
+  if(!user) {
+    return <AuthPage onAuth={(user) => setuser(user)} />;
+  } else {
+    return <ChatsPage user ={user} /> ;
+  }
   return (
     <div>App</div>
   )
